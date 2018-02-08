846eb121b6b9bab1afa857f48897f03256d0b6e7
Yurina Shin <yurinas@gmail.com>
2018-02-08 23:32:50 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   end
 
   def patients
-    @appointments.collect do |appointment| appointment.patinet
+    @appointments.collect do |appointment| appointment.patient
     end
   end
 end
